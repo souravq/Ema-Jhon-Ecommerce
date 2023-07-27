aab9267ab11be472d217a1fea8bf4f2da698b969
souravq <souravbera515@gmail.com>
2023-07-27 17:15:34 +0530

    Shop component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './component/Header/Header'
+import Shop from './component/Shop/Shop'
 
 function App() {
 
   return (
     <>
      <Header></Header>
+     <Shop></Shop>
     </>
   )
 }
